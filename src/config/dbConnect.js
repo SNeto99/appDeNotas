ee4768f7c4d5f78cb55b3d3b07fb4bbc7649385f
SNeto99 <netoseno@hotmail.com>
2024-08-04 19:53:04 -0300

    atualização banco

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,27 +1,53 @@
 import mysql from "mysql";
 import { config } from "dotenv";
 
-const host = process.env.HOST
-const user = process.env.USER
-const password = process.env.PASSWORD
-const database = process.env.DATABASE
+config(); // Esta linha assegura que as variáveis de ambiente sejam carregadas
 
 const dadosBanco = {
-    host: host,
-    user: user,
-    password: password,
-    database: database,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 };
 
-const banco  = mysql.createConnection(dadosBanco);
+let banco; 
+let tentativas = 0;
+const maximoTentativas = 10; 
 
-// Conectar ao banco de dados
-banco.connect((err) => {
-    if (err) {
-        return console.error("Erro ao conectar: " + err.message);
-    }
-    console.log("Conectado ao banco de dados MySQL!");
-});
+function handleConnection() {
+    banco = mysql.createConnection(dadosBanco);
 
+    banco.connect(err => {
+        if (err) {
+            console.error("Erro ao conectar: " + err.message);
+            tentativas++;
+            if (tentativas <= maximoTentativas) {
+                setTimeout(handleDisconnect, 2000); // Tentar reconectar após 2 segundos
+            } else {
+                console.error("Falha ao reconectar após " + maximoTentativas + " tentativas.");
+            }
+        } else {
+            console.log("Conectado ao banco de dados MySQL!");
+            tentativas = 0;
+        }
+    });
+
+    banco.on('error', function (err) {
+        console.log('Erro de banco de dados:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') {
+            console.error('A conexão foi interrompida.');
+            if (tentativas <= maximoTentativas) {
+                handleConnection();
+            } else {
+                console.error("Falha ao reconectar após " + maximoTentativas + " tentativas.");
+            }
+        } else {
+            console.error('Erro no banco de dados:', err);
+            throw err;
+        }
+    });
+}
+
+handleConnection();
 
 export default banco;
